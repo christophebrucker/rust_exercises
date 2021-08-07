bf999c12a6939cd31c61f0ddb410eda31e1d2f09
Christophe Brucker <christophe.brucker@gmail.com>
2021-08-07 16:40:12 +0200

    Exercises

--- src/functions.rs
+++ src/functions.rs
@@ -16,3 +16,43 @@ pub fn sort(array: [i32; 4]) {
   array_copy.sort();
   println!("The array sorted is {:?}", array_copy);
 }
+
+// remove a certain character from a string
+pub fn remove(string: &str, character: &str) {
+ println!("{:?}", string.replace(character, ""));
+}
+
+// sum all numbers of an array
+pub fn sum(array: [i32; 10]) {
+  let mut sum = 0;
+  for n in 0..array.len() {
+    sum += array[n];
+  }
+  println!("The sum of the values of the array is {}", sum);
+}
+
+// chunk vectors
+pub fn chunk(vector: Vec<usize>, number: usize) {
+  let number_of_chunks: usize;
+  if vector.len() % number == 0 {
+    number_of_chunks = vector.len() / number;
+  } else {
+    number_of_chunks = vector.len() / number + 1;
+   }
+  println!("{}", number_of_chunks);
+  let mut count = 0;
+  for x in 0..number_of_chunks {
+    // create a new vector with the "variable number" length
+    let mut new_vec: Vec<usize> = Vec::new();
+    while new_vec.len() < number {
+      if count > vector.len() - 1 {
+        break;
+      }
+      new_vec.push(vector[count]);
+      count += 1;
+    }
+    println!("{:?}", new_vec);
+  }
+
+  //for i in 0..array.len()
+}
--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,13 @@ mod functions;
 fn main() {
     //functions::reverse("Hello, world!");
     //functions::max(34, 32);
-    functions::sort([4,3,2,1]);
+    //functions::sort([4,3,2,1]);
+    //functions::remove("test", "t");
+    //functions::sum([10,10,10,10,10,10,10,10,10,10]);
+    functions::chunk(vec![1,2,3,4],3);
 }
 
 /*
-
-// (D) remove a given character from a string
-// TESTS:
-// removeChar("Hello World", "H")            // => "ello World"
-// removeChar("Hi, how are you doing?", "i") // => "H, how are you dong?"
-function removeChar(str, char) {
-  return str.split(char).join();
-}
-
-// (E) sum all numbers of an array
-// TESTS:
-// sum([1,2,3,4,5])      // => 15
-// sum([829,-12,758,2])  // => 1577
-function sum(arr) {
-  return arr.reduce((a,b) => a + b);
-}
-
-// (F) chunk an array into an array of arrays
-// => what data structur would you use for that in Rust
-// => can you even create an Array of Arrays in Rust?
-// TESTS:
-// chunkArray([1,2,3,4,5,1,2,3,4,5,1,2,3], 5)
-//        => [[1,2,3,4,5],[1,2,3,4,5],[1,2,3]]
-// chunkArray([1,2,3,4,5,1,2,3,4,5,1,2,3], 3)
-//        => [[1,2,3],[4,5,1],[2,3,4],[5,1,2],[3]]
-function chunkArray(arr, size) {
-  const chunkedArr = [];
-  let index = 0;
-  while (index < array.length) {
-    chunkedArr.push(array.slice(index, size + index));
-    index += size;
-  }
-  return chunkedArr;
-}
 
 // (G) count occurencies all distinct values of an array and return as dictionary
 // => what datastructure to use in RUST for such a task?
